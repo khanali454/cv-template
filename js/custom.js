debc3742d3d58ac49f9eefc73d26ffc664e1d7b4
khanali454 <khanandkhan061@gmail.com>
2023-09-16 19:14:53 +0500

    js added

--- js/custom.js
+++ js/custom.js
@@ -1,38 +1,38 @@
 fetch("response.json")
-.then((res) => {
-return res.json();
-})
-.then((jsonData) => {
+    .then((res) => {
+        return res.json();
+    })
+    .then((jsonData) => {
 
-    writeCV(jsonData);
-// Personal Info
-document.querySelector('.value.nationality').textContent = jsonData.basics.nationality;
-document.querySelector('.value.nric').textContent = jsonData.basics.nric;
-document.querySelector('.value.age').textContent = jsonData.basics.age;
-document.querySelector('.value.gender').textContent = jsonData.basics.gender;
-document.querySelector('.value.maritalStatus').textContent = jsonData.basics.maritalStatus;
-document.querySelector('.value.dob').textContent = jsonData.basics.dob;
+        writeCV(jsonData);
+        // Personal Info
+        document.querySelector('.value.nationality').textContent = jsonData.basics.nationality;
+        document.querySelector('.value.nric').textContent = jsonData.basics.nric;
+        document.querySelector('.value.age').textContent = jsonData.basics.age;
+        document.querySelector('.value.gender').textContent = jsonData.basics.gender;
+        document.querySelector('.value.maritalStatus').textContent = jsonData.basics.maritalStatus;
+        document.querySelector('.value.dob').textContent = jsonData.basics.dob;
 
-document.querySelector('.value.phone').textContent = jsonData.basics.phone;
-document.querySelector('.value.email').textContent = jsonData.basics.email;
-document.querySelector('.value.linkedin').textContent = jsonData.basics.linkedinProfile;
+        document.querySelector('.value.phone').textContent = jsonData.basics.phone;
+        document.querySelector('.value.email').textContent = jsonData.basics.email;
+        document.querySelector('.value.linkedin').textContent = jsonData.basics.linkedinProfile;
 
-var addressLines = jsonData.basics.location.address.split('\n');
-var addressHTML = '';
-addressLines.forEach(function (line) {
-addressHTML += '<div class="line">' + line + '</div>';
-});
-document.querySelector('.value.address').innerHTML = addressHTML;
+        var addressLines = jsonData.basics.location.address.split('\n');
+        var addressHTML = '';
+        addressLines.forEach(function (line) {
+            addressHTML += '<div class="line">' + line + '</div>';
+        });
+        document.querySelector('.value.address').innerHTML = addressHTML;
 
-document.querySelector('.person-name').textContent = jsonData.basics.name;
+        document.querySelector('.person-name').textContent = jsonData.basics.name;
 
-// Summary
-document.querySelector('.s__content').textContent = jsonData.projects[0].description; // Assuming the first project description is the summary
+        // Summary
+        document.querySelector('.s__content').textContent = jsonData.projects[0].description; // Assuming the first project description is the summary
 
-// Experience
-var experiencesContainer = document.querySelector('.__experiences');
-jsonData.work.forEach(function (experience) {
-var experienceHTML = `
+        // Experience
+        var experiencesContainer = document.querySelector('.__experiences');
+        jsonData.work.forEach(function (experience) {
+            var experienceHTML = `
     <div class="__experience">
         <div class="flex-item">
             <div class="left_item">
@@ -49,19 +49,19 @@ var experienceHTML = `
         <div class="highlights">
             <ul class="list">
                 ${experience.highlights.map(function (highlight) {
-    return `<li>${highlight}</li>`;
-}).join('')}
+                return `<li>${highlight}</li>`;
+            }).join('')}
             </ul>
         </div>
     </div>
 `;
-experiencesContainer.innerHTML += experienceHTML;
-});
+            experiencesContainer.innerHTML += experienceHTML;
+        });
 
-// Education
-var educationContainer = document.querySelector('.education_degrees');
-jsonData.education.forEach(function (education) {
-var educationHTML = `
+        // Education
+        var educationContainer = document.querySelector('.education_degrees');
+        jsonData.education.forEach(function (education) {
+            var educationHTML = `
     <div class="ed_degree">
         <div class="ed_detail">
             <b class="university_name">${education.institution}</b>
@@ -78,23 +78,23 @@ var educationHTML = `
         </div>
     </div>
 `;
-educationContainer.innerHTML += educationHTML;
-});
+            educationContainer.innerHTML += educationHTML;
+        });
 
-// Skills
-document.querySelector('.all_skils').textContent = jsonData.skills.map(function (skill) {
-return skill.name;
-}).join(', ');
+        // Skills
+        document.querySelector('.all_skils').textContent = jsonData.skills.map(function (skill) {
+            return skill.name;
+        }).join(', ');
 
-// Job Preferences
-document.querySelector('.availability_date').textContent = jsonData.jobPreferences.dateOfAvailability;
-document.querySelector('.previous_salary').textContent = jsonData.jobPreferences.previousSalary;
-document.querySelector('.expected_salary').textContent = jsonData.jobPreferences.expectedSalary;
+        // Job Preferences
+        document.querySelector('.availability_date').textContent = jsonData.jobPreferences.dateOfAvailability;
+        document.querySelector('.previous_salary').textContent = jsonData.jobPreferences.previousSalary;
+        document.querySelector('.expected_salary').textContent = jsonData.jobPreferences.expectedSalary;
 
-// Referees
-var refereesContainer = document.querySelector('.refrees_list');
-jsonData.references.forEach(function (reference) {
-var referenceHTML = `
+        // Referees
+        var refereesContainer = document.querySelector('.refrees_list');
+        jsonData.references.forEach(function (reference) {
+            var referenceHTML = `
     <div class="referee">
         <div class="name">${reference.name}</div>
         <div class="designation">${reference.position}</div>
@@ -106,14 +106,16 @@ var referenceHTML = `
         </div>
     </div>
 `;
-refereesContainer.innerHTML += referenceHTML;
-});
+            refereesContainer.innerHTML += referenceHTML;
+        });
 
 
 
-});
+    });
 
 
-function writeCV(resp){
-console.log(resp);
+function writeCV(resp) {
+    if ('basics' in resp) {
+        insertBasics();
+    }
 }
